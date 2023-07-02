1a8f2504e2269fc460e8afc884aee203de598a0f
yuhuanowo <huhu11256@gmail.com>
2023-07-02 19:49:15 +0800

    modified:   cogs/owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -20,9 +20,9 @@ class Owner(commands.Cog, name="owner"):
 
     @commands.command(
         name="sync",
-        description="Synchonizes the slash commands.",
+        description="同步斜杠命令.",
     )
-    @app_commands.describe(scope="The scope of the sync. Can be `global` or `guild`")
+    @app_commands.describe(scope="同步範圍. 可以是 `global` or `guild`(全域或伺服器)")
     @checks.is_owner()
     async def sync(self, context: Context, scope: str) -> None:
         """
@@ -35,31 +35,37 @@ class Owner(commands.Cog, name="owner"):
         if scope == "global":
             await context.bot.tree.sync()
             embed = discord.Embed(
-                description="Slash commands have been globally synchronized.",
+                title="斜杠命令已全局同步.",
                 color=0x9C84EF,
             )
+            embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+            embed.timestamp = context.message.created_at
             await context.send(embed=embed)
             return
         elif scope == "guild":
             context.bot.tree.copy_global_to(guild=context.guild)
             await context.bot.tree.sync(guild=context.guild)
             embed = discord.Embed(
-                description="Slash commands have been synchronized in this guild.",
+                title="本公會已同步斜線指令.",
                 color=0x9C84EF,
             )
+            embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+            embed.timestamp = context.message.created_at
             await context.send(embed=embed)
             return
         embed = discord.Embed(
-            description="The scope must be `global` or `guild`.", color=0xE02B2B
+            title="範圍必須是`global` or `guild`.", color=0xE02B2B
         )
+        embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+        embed.timestamp = context.message.created_at
         await context.send(embed=embed)
 
     @commands.command(
         name="unsync",
-        description="Unsynchonizes the slash commands.",
+        description="不同步斜杠命令.",
     )
     @app_commands.describe(
-        scope="The scope of the sync. Can be `global`, `current_guild` or `guild`"
+        scope="同步的範圍。可以是 `global`, `current_guild` or `guild` (全域、當前伺服器或伺服器)"
     )
     @checks.is_owner()
     async def unsync(self, context: Context, scope: str) -> None:
@@ -74,30 +80,36 @@ class Owner(commands.Cog, name="owner"):
             context.bot.tree.clear_commands(guild=None)
             await context.bot.tree.sync()
             embed = discord.Embed(
-                description="Slash commands have been globally unsynchronized.",
+                title="斜杠命令已設為全域不同步。",
                 color=0x9C84EF,
             )
+            embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+            embed.timestamp = context.message.created_at
             await context.send(embed=embed)
             return
         elif scope == "guild":
             context.bot.tree.clear_commands(guild=context.guild)
             await context.bot.tree.sync(guild=context.guild)
             embed = discord.Embed(
-                description="Slash commands have been unsynchronized in this guild.",
+                title="該伺服器中的斜杠命令已不同步.",
                 color=0x9C84EF,
             )
+            embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+            embed.timestamp = context.message.created_at
             await context.send(embed=embed)
             return
         embed = discord.Embed(
-            description="The scope must be `global` or `guild`.", color=0xE02B2B
+            title="範圍必須是 `global` or `guild` (全域或伺服器).", color=0xE02B2B
         )
+        embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+        embed.timestamp = context.message.created_at
         await context.send(embed=embed)
 
     @commands.hybrid_command(
         name="load",
         description="Load a cog",
     )
-    @app_commands.describe(cog="The name of the cog to load")
+    @app_commands.describe(cog="要加載的cog名稱")
     @checks.is_owner()
     async def load(self, context: Context, cog: str) -> None:
         """
@@ -110,20 +122,24 @@ class Owner(commands.Cog, name="owner"):
             await self.bot.load_extension(f"cogs.{cog}")
         except Exception:
             embed = discord.Embed(
-                description=f"Could not load the `{cog}` cog.", color=0xE02B2B
+                title=f"無法加載 `{cog}` cog.", color=0xE02B2B
             )
+            embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+            embed.timestamp = context.message.created_at
             await context.send(embed=embed)
             return
         embed = discord.Embed(
-            description=f"Successfully loaded the `{cog}` cog.", color=0x9C84EF
+            title=f"成功加載 `{cog}` cog.", color=0x9C84EF
         )
+        embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+        embed.timestamp = context.message.created_at
         await context.send(embed=embed)
 
     @commands.hybrid_command(
         name="unload",
         description="Unloads a cog.",
     )
-    @app_commands.describe(cog="The name of the cog to unload")
+    @app_commands.describe(cog="要卸載的cog名稱")
     @checks.is_owner()
     async def unload(self, context: Context, cog: str) -> None:
         """
@@ -136,20 +152,24 @@ class Owner(commands.Cog, name="owner"):
             await self.bot.unload_extension(f"cogs.{cog}")
         except Exception:
             embed = discord.Embed(
-                description=f"Could not unload the `{cog}` cog.", color=0xE02B2B
+                title=f"無法卸載 `{cog}` cog.", color=0xE02B2B
             )
+            embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+            embed.timestamp = context.message.created_at
             await context.send(embed=embed)
             return
         embed = discord.Embed(
-            description=f"Successfully unloaded the `{cog}` cog.", color=0x9C84EF
+            title=f"成功卸載了 `{cog}` cog.", color=0x9C84EF
         )
+        embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+        embed.timestamp = context.message.created_at
         await context.send(embed=embed)
 
     @commands.hybrid_command(
         name="reload",
         description="Reloads a cog.",
     )
-    @app_commands.describe(cog="The name of the cog to reload")
+    @app_commands.describe(cog="要重新加載的cog名稱")
     @checks.is_owner()
     async def reload(self, context: Context, cog: str) -> None:
         """
@@ -162,18 +182,22 @@ class Owner(commands.Cog, name="owner"):
             await self.bot.reload_extension(f"cogs.{cog}")
         except Exception:
             embed = discord.Embed(
-                description=f"Could not reload the `{cog}` cog.", color=0xE02B2B
+                title=f"無法重新加載 `{cog}` cog.", color=0xE02B2B
             )
+            embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+            embed.timestamp = context.message.created_at
             await context.send(embed=embed)
             return
         embed = discord.Embed(
-            description=f"Successfully reloaded the `{cog}` cog.", color=0x9C84EF
+            title=f"重新加載成功 `{cog}` cog.", color=0x9C84EF
         )
+        embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+        embed.timestamp = context.message.created_at
         await context.send(embed=embed)
 
     @commands.hybrid_command(
         name="shutdown",
-        description="Make the bot shutdown.",
+        description="讓歸終關閉.",
     )
     @checks.is_owner()
     async def shutdown(self, context: Context) -> None:
@@ -182,15 +206,17 @@ class Owner(commands.Cog, name="owner"):
 
         :param context: The hybrid command context.
         """
-        embed = discord.Embed(description="Shutting down. Bye! :wave:", color=0x9C84EF)
+        embed = discord.Embed(description="正在關閉.... 再見! :wave:", color=0x9C84EF)
+        embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+        embed.timestamp = context.message.created_at
         await context.send(embed=embed)
         await self.bot.close()
 
     @commands.hybrid_command(
         name="say",
-        description="The bot will say anything you want.",
+        description="歸終會說你想說的任何話.",
     )
-    @app_commands.describe(message="The message that should be repeated by the bot")
+    @app_commands.describe(message="你想歸終重複的消息")
     @checks.is_owner()
     async def say(self, context: Context, *, message: str) -> None:
         """
@@ -203,9 +229,9 @@ class Owner(commands.Cog, name="owner"):
 
     @commands.hybrid_command(
         name="embed",
-        description="The bot will say anything you want, but within embeds.",
+        description="歸終會說任何你想說的話，但在嵌入範圍內.",
     )
-    @app_commands.describe(message="The message that should be repeated by the bot")
+    @app_commands.describe(message="你想歸終重複的消息")
     @checks.is_owner()
     async def embed(self, context: Context, *, message: str) -> None:
         """
@@ -219,7 +245,7 @@ class Owner(commands.Cog, name="owner"):
 
     @commands.hybrid_group(
         name="blacklist",
-        description="Get the list of all blacklisted users.",
+        description="獲取所有黑名單用戶列表s.",
     )
     @checks.is_owner()
     async def blacklist(self, context: Context) -> None:
@@ -230,15 +256,17 @@ class Owner(commands.Cog, name="owner"):
         """
         if context.invoked_subcommand is None:
             embed = discord.Embed(
-                description="You need to specify a subcommand.\n\n**Subcommands:**\n`add` - Add a user to the blacklist.\n`remove` - Remove a user from the blacklist.",
+                title="您需要指定一個子命令.\n\n**子命令:**\n`add` - 將用戶添加到黑名單.\n`remove` - 將用戶從黑名單中刪除。",
                 color=0xE02B2B,
             )
+            embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+            embed.timestamp = context.message.created_at
             await context.send(embed=embed)
 
     @blacklist.command(
         base="blacklist",
         name="show",
-        description="Shows the list of all blacklisted users.",
+        description="顯示所有列入黑名單的用戶列表.",
     )
     @checks.is_owner()
     async def blacklist_show(self, context: Context) -> None:
@@ -250,27 +278,31 @@ class Owner(commands.Cog, name="owner"):
         blacklisted_users = await db_manager.get_blacklisted_users()
         if len(blacklisted_users) == 0:
             embed = discord.Embed(
-                description="There are currently no blacklisted users.", color=0xE02B2B
+                title="目前沒有黑名單用戶.", color=0xE02B2B
             )
+            embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+            embed.timestamp = context.message.created_at
             await context.send(embed=embed)
             return
 
-        embed = discord.Embed(title="Blacklisted Users", color=0x9C84EF)
+        embed = discord.Embed(title="列入黑名單的用戶", color=0x9C84EF)
         users = []
         for bluser in blacklisted_users:
             user = self.bot.get_user(int(bluser[0])) or await self.bot.fetch_user(
                 int(bluser[0])
             )
-            users.append(f"• {user.mention} ({user}) - Blacklisted <t:{bluser[1]}>")
+            users.append(f"• {user.mention} ({user}) - 列入黑名單 <t:{bluser[1]}>")
         embed.description = "\n".join(users)
+        embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+        embed.timestamp = context.message.created_at
         await context.send(embed=embed)
 
     @blacklist.command(
         base="blacklist",
         name="add",
-        description="Lets you add a user from not being able to use the bot.",
+        description="允許您添加無法使用歸終的用戶.",
     )
-    @app_commands.describe(user="The user that should be added to the blacklist")
+    @app_commands.describe(user="想添加到黑名單的用戶")
     @checks.is_owner()
     async def blacklist_add(self, context: Context, user: discord.User) -> None:
         """
@@ -282,27 +314,28 @@ class Owner(commands.Cog, name="owner"):
         user_id = user.id
         if await db_manager.is_blacklisted(user_id):
             embed = discord.Embed(
-                description=f"**{user.name}** is already in the blacklist.",
+                title=f"**{user.name}** 已經在黑名單裡了。",
                 color=0xE02B2B,
             )
+            embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+            embed.timestamp = context.message.created_at
             await context.send(embed=embed)
             return
         total = await db_manager.add_user_to_blacklist(user_id)
         embed = discord.Embed(
-            description=f"**{user.name}** has been successfully added to the blacklist",
+            title=f"**{user.name}** 已成功加入黑名單",
             color=0x9C84EF,
         )
-        embed.set_footer(
-            text=f"There {'is' if total == 1 else 'are'} now {total} {'user' if total == 1 else 'users'} in the blacklist"
-        )
+        embed.set_footer(text="那裡 {'有' if total == 1 else '有'} {total} {'個用戶' if total == 1 else '個用戶'} 在黑名單中",icon_url=self.bot.user.avatar.url)
+        embed.timestamp = context.message.created_at
         await context.send(embed=embed)
 
     @blacklist.command(
         base="blacklist",
         name="remove",
-        description="Lets you remove a user from not being able to use the bot.",
+        description="允許您刪除無法使用歸終的用戶.",
     )
-    @app_commands.describe(user="The user that should be removed from the blacklist.")
+    @app_commands.describe(user="應從黑名單中刪除的用戶.")
     @checks.is_owner()
     async def blacklist_remove(self, context: Context, user: discord.User) -> None:
         """
@@ -314,18 +347,19 @@ class Owner(commands.Cog, name="owner"):
         user_id = user.id
         if not await db_manager.is_blacklisted(user_id):
             embed = discord.Embed(
-                description=f"**{user.name}** is not in the blacklist.", color=0xE02B2B
+                title=f"**{user.name}** 不在黑名單中.", color=0xE02B2B
             )
+            embed.set_footer(text="可愛的歸終~",icon_url=self.bot.user.avatar.url)
+            embed.timestamp = context.message.created_at
             await context.send(embed=embed)
             return
         total = await db_manager.remove_user_from_blacklist(user_id)
         embed = discord.Embed(
-            description=f"**{user.name}** has been successfully removed from the blacklist",
+            title=f"**{user.name}** 已成功移出黑名單",
             color=0x9C84EF,
         )
-        embed.set_footer(
-            text=f"There {'is' if total == 1 else 'are'} now {total} {'user' if total == 1 else 'users'} in the blacklist"
-        )
+        embed.set_footer(text="那裡 {'有' if total == 1 else '有'} {total} {'個用戶' if total == 1 else '個用戶'} 在黑名單中",icon_url=self.bot.user.avatar.url)
+        embed.timestamp = context.message.created_at
         await context.send(embed=embed)
 
 
